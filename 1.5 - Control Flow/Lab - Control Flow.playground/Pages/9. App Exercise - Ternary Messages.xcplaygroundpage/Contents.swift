b2d6ae47eb1ea08796a132c3ac01e5dae55e30d9
Nicolas Garaycochea <nicolas.garaycochea@gmail.com>
2022-11-09 11:27:08 -0700

    finished day 2

--- 1.5 - Control Flow/Lab - Control Flow.playground/Pages/9. App Exercise - Ternary Messages.xcplaygroundpage/Contents.swift
+++ 1.5 - Control Flow/Lab - Control Flow.playground/Pages/9. App Exercise - Ternary Messages.xcplaygroundpage/Contents.swift
@@ -6,13 +6,15 @@
  The code below should look similar to code you wrote in the Fitness Decisions exercise. The if-else statement is actually unnecessary, and instead you can print either one statement or the other all on one line using the ternary operator. Go ahead and refactor the code below to do just that.
  */
 let stepGoal = 10000
-let steps = 3948
+let steps = 6000
 
-if steps < stepGoal / 2 {
-    print("Almost halfway!")
-} else {
-    print("Over halfway!")
-}
+//if steps < stepGoal / 2 {
+//    print("Almost halfway!")
+//} else {
+//    print("Over halfway!")
+//}
+
+steps < stepGoal/2 ? print("Almost halfway!") : print("Over halfway!")
 /*:
  _Copyright © 2021 Apple Inc._
 
