48c8bbe31524dcfad44f082f7e7757cf702de072
Nicolas Garaycochea <nicolas.garaycochea@gmail.com>
2022-11-10 08:08:34 -0700

    added IBDemo

---
+++ IBDemo/IBDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  IBDemo
+//
+//  Created by Nicolas Garaycochea on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLable: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
